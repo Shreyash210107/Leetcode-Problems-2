8ecc07eb7c470064216e479294afdd2da198881d
Shreyash210107 <patilshreyash210107@gmail.com>
2026-09-05 22:48:43 +0530

    Add files via upload

--- SmallestStableIndex.java
+++ SmallestStableIndex.java
@@ -1,25 +1,21 @@
-
 import java.util.*;
 
 public class SmallestStableIndex {
-
-    public static int findStableIndex(int[] nums, int k) {
+    public static int smallestStableIndex(int[] nums, int k) {
         int n = nums.length;
 
         // Find suffix minimum
         int[] suffixMin = new int[n];
-
         suffixMin[n - 1] = nums[n - 1];
 
         for (int i = n - 2; i >= 0; i--) {
             suffixMin[i] = Math.min(nums[i], suffixMin[i + 1]);
         }
 
-        // Find the first stable index
+        // Find smallest stable index
         int prefixMax = nums[0];
 
         for (int i = 0; i < n; i++) {
-
             prefixMax = Math.max(prefixMax, nums[i]);
 
             int instability = prefixMax - suffixMin[i];
@@ -33,28 +29,9 @@ public class SmallestStableIndex {
     }
 
     public static void main(String[] args) {
+        int[] nums = {5, 0, 1, 4};
+        int k = 3;
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter number of elements: ");
-        int n = sc.nextInt();
-
-        int[] nums = new int[n];
-
-        System.out.println("Enter array elements:");
-
-        for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
-        }
-
-        System.out.print("Enter k: ");
-        int k = sc.nextInt();
-
-        int answer = findStableIndex(nums, k);
-
-        System.out.println("Smallest stable index: " + answer);
-
-        sc.close();
+        System.out.println(smallestStableIndex(nums, k));
     }
 }
-
